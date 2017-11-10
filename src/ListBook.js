0440ad7b4a5a8740d6c7b92a877639fdd7d8f4bc
Pablo <paul.ladiv123@gmail.com>
2017-11-10 08:52:27 +0000

    Initial setup : npm installed dependencies, add ListBook compont w/ div

---
+++ src/ListBook.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListBook extends Component {
+
+
+  render() {
+
+    return (
+      <div> Initial setup </div>
+    )
+  }
+}
+
+export default ListBook
