02bdf33c2045a168dec3b40d0ce3fc1708b1e15a
Pablo <paul.ladiv123@gmail.com>
2017-11-25 18:12:11 +0000

    feature:  Book component child of ListBook component. Need shelf filter

--- src/Book.js
+++ src/Book.js
@@ -7,10 +7,9 @@ import * as BooksAPI from './BooksAPI'
 
 function Book(props) {
   return (
-
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193,  }}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.imageBackground})` }}></div>
         <div className="book-shelf-changer">
           <select>
             <option value="none" disabled>Move to...</option>
@@ -21,11 +20,9 @@ function Book(props) {
           </select>
         </div>
       </div>
-      <div className="book-title">{props.title}</div>
+      <div className="book-title">{props.title} </div>
       <div className="book-authors">{props.authors}</div>
     </div>
-
-
   );
 }
 
--- src/ListBook.js
+++ src/ListBook.js
@@ -11,6 +11,7 @@ class ListBook extends Component {
   constructor(props){
      super(props);
      this.state = {
+title: ""
 
      };
 
@@ -19,8 +20,6 @@ class ListBook extends Component {
   render() {
 
 const { books } = this.props
-
-console.log("something")
 
 let showingBooks = books
 
@@ -33,25 +32,16 @@ let showingBooks = books
 <div>
 <ol className="books-grid">
 
-    {showingBooks.map((book) => (
 
-        <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
-        </div>
+
+    {showingBooks.map((book) => (
+        <li key = {book.id}>
+        <Book
+          title = {book.title}
+          authors = {book.authors}
+          imageBackground = {book.imageLinks.smallThumbnail}
+
+        />
         </li>
 
     ))}
